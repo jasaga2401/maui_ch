c3c81bef2b1a42eb001d0630dec8cf3796b4711b
jasaga2401 <jasaga@protonmail.com>
2024-01-24 22:24:25 +0000

    Add project files.

---
+++ maui_ch/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_ch
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ maui_ch/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microcharts;
+using Microcharts.Maui;
+using SkiaSharp;
+
+namespace maui_ch
+{
+    public partial class MainPage : ContentPage
+    {       
+        
+        ChartEntry[] entries = new[]
+            {
+                new ChartEntry(200)
+                {
+                    Label = "January",
+                    ValueLabel = "200",
+                    Color = SKColor.Parse("#266489")
+                },
+                new ChartEntry(400)
+                {
+                    Label = "February",
+                    ValueLabel = "400",
+                    Color = SKColor.Parse("#68B9C0")
+                },
+                new ChartEntry(-100)
+                {
+                    Label = "March",
+                    ValueLabel = "-100",
+                    Color = SKColor.Parse("#90D585")
+                }
+            };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            chartView.Chart = new BarChart() { Entries = entries };
+        }       
+    }
+}
